9e24058a8e0ad0ad5085f63033c4196ebf1682ff
Michelle <jiahu0724417@gmail.com>
2025-04-30 20:33:32 +0800

    調整bootstrap連結

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 // 匯入 SCSS 樣式
 import './assets/scss/all.scss';
 
-// 匯入第三方套件的 CSS
-import 'nouislider/dist/nouislider.css';
-import 'aos/dist/aos.css'; 
-
 // 匯入 Bootstrap 的 JS
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// 匯入 AOS 動畫套件
-import AOS from 'aos'
 
 // 匯入專案的 JS 檔案
 import './js/homeTopChoiceSwiper';
@@ -18,6 +13,16 @@ import './js/searchzoneInput';
 import './js/slider';
 import './js/loginButton';
 import './js/detailsForm';
+
+// 匯入第三方套件的 CSS
+import 'nouislider/dist/nouislider.css';
+import 'aos/dist/aos.css';
+
+
+// 匯入 AOS 動畫套件
+import AOS from 'aos';
+
+
 
 // 初始化 AOS
 AOS.init();
